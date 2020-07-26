97a69e895b1b525bbcedd717a37dd903e16d22ad
kbj <kbj@wingeat.com>
2020-07-26 15:41:29 +0900

    feat: test example about typeorm mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PetsController } from './pets/pets.controller';
+import { Pet } from './pets/pet.entity';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.DB_CONNECTION_STRING,
+      entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+      synchronize: true,
+      useNewUrlParser: true,
+      logging: true,
+      useUnifiedTopology: true
+    }),
+    TypeOrmModule.forFeature([Pet]),
+  ],
+  controllers: [AppController, PetsController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/pets/pets.controller.spec.ts
@@ -0,0 +1,35 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PetsController } from './pets.controller';
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { join } from "path";
+import { Pet } from "./pet.entity";
+
+describe('Pets Controller', () => {
+  let controller: PetsController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [PetsController],
+      imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+          type: 'mongodb',
+          url: process.env.DB_CONNECTION_STRING,
+          entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+          synchronize: true,
+          useNewUrlParser: true,
+          logging: true,
+          useUnifiedTopology: true
+        }),
+        TypeOrmModule.forFeature([Pet]),
+      ]
+    }).compile();
+
+    controller = module.get<PetsController>(PetsController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
